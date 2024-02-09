34fb10ee85ce67d11179156c5a7a4edcfe0be48e
Renan Reis Martins de Paula <renanreismartins@gmail.com>
2024-02-09 23:40:07 +0000

    Preparation Phase: Re-order fields and add event dates to the model.

--- java/src/main/java/refactoring/domain/Order.java
+++ java/src/main/java/refactoring/domain/Order.java
@@ -1,14 +1,27 @@
 package refactoring.domain;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Order {
+    private int id;
     private Double total;
     private String currency;
     private List<OrderItem> items;
     private Double tax;
     private OrderStatus status;
-    private int id;
+    private LocalDateTime creationDate;
+    private LocalDateTime approvalDate;
+    private LocalDateTime shipmentDate;
+    private LocalDateTime rejectionDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public Double getTotal() {
         return total;
@@ -50,11 +63,35 @@ public class Order {
         this.status = status;
     }
 
-    public int getId() {
-        return id;
+    public LocalDateTime getCreationDate() {
+        return creationDate;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getApprovalDate() {
+        return approvalDate;
+    }
+
+    public void setApprovalDate(LocalDateTime approvalDate) {
+        this.approvalDate = approvalDate;
+    }
+
+    public LocalDateTime getShipmentDate() {
+        return shipmentDate;
+    }
+
+    public void setShipmentDate(LocalDateTime shipmentDate) {
+        this.shipmentDate = shipmentDate;
+    }
+
+    public LocalDateTime getRejectionDate() {
+        return rejectionDate;
+    }
+
+    public void setRejectionDate(LocalDateTime rejectionDate) {
+        this.rejectionDate = rejectionDate;
     }
 }
